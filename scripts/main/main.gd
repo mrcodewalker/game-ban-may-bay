27e6ebd4d282ed4bee49f81c46b88869d33b3a55
Huynh Ngoc Hai <mr.codewalker@gmail.com>
2026-08-24 01:23:04 +0700

    Revert Mission Briefing cutscene so game enters gameplay directly

--- scripts/main/main.gd
+++ scripts/main/main.gd
@@ -1,11 +1,5 @@
 extends Node2D
 
-@export var briefing_scene: PackedScene = preload("res://scenes/ui/mission_briefing.tscn")
-
 func _ready() -> void:
 	if AudioManager:
 		AudioManager.play_bgm("bgm_main")
-		
-	if briefing_scene:
-		var briefing = briefing_scene.instantiate()
-		add_child(briefing)
